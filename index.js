3dd87362b489afda4b5224ae91e7a89fc5494b22
sproket176 <37910399+sproket176@users.noreply.github.com>
2018-03-31 14:59:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 const bot = new Discord.Client();
 const token = 'NDI4MjA5NTEzOTc2Mjk5NTIx.DZvxow.yhNgrYpHmy9lb2cc2M2RUEpMSMM';
 const server_id = '401424715954126858';
@@ -6,11 +7,22 @@ const spam_channel = '401428462671364097';
 const info_channel = '407592712963883008';
 const cmd_channel = '428256083690127371';
 
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if (server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    });
+}
+var servers = {};
+
   bot.on('ready', async () => {
     console.log(`Logged in as ${bot.user.tag}!`);
     bot.user.setActivity('prezydenta', { type: 'LISTENING' });
     var channel = bot.channels.get(cmd_channel);
-    channel.send('```KOMENDY:\n/say [tekst] - pisanie na spamowej\n/info [tekst] - pisanie na informacyjnej```');
+    channel.send('```KOMENDY BOTA:\n/say [tekst] - pisanie na spamowej\n/info [tekst] - pisanie na informacyjnej``````KOMENDY MUZYCZNE:\n/play [youtube url] - można dodać kilka piosenek pod rząd\n/skip - pominięcie utworu i odtwarzanie następnego w kolejce\n/stop - zatrzymanie muzyki i kick bota z kanału```');
 });
 
   bot.on('guildMemberAdd', member => {
@@ -61,6 +73,46 @@ if(cmd === '/info') {
     return;
 }
 }
+
+if(cmd === '/play') {
+    if (message.channel.id === cmd_channel) {
+    if(!args[0]) {
+        message.delete().catch();
+        return;
+    }
+    if(!message.member.voiceChannel) {
+        message.member.send("Musisz być na kanale głosowym.");
+        message.delete().catch();
+        return;
+    }
+    if(!servers[message.guild.id]) servers[message.guild.id] = {
+        queue: []
+    };
+      var server = servers[message.guild.id];
+
+    server.queue.push(args[0]);
+
+      if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+        play(connection, message);
+            message.delete().catch();
+      });
+    }
+}
+
+    if(cmd === '/skip') {
+        if (message.channel.id === cmd_channel) {
+        var server = servers[message.guild.id];
+        message.delete().catch();
+        if(server.dispatcher) server.dispatcher.end();
+        }
+    }
+    if(cmd === '/stop') {
+        if (message.channel.id === cmd_channel) {
+            var server = servers[message.guild.id];
+            message.delete().catch();
+            if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+            }
+        }
   });
 
 bot.login(token);
